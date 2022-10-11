c1865daf4672f39eb60acd5dced71356aa231f19
Emil Shafeev <amil0512@gmail.com>
2022-10-11 13:46:27 +0500

    - new feature-1 - new tests 3 and 4

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,6 +3,12 @@ describe('some describe', () => {
 		expect(1).toBe(1)
 	})
 	test('some test 2', () => {
-		expect(2).not.toBe(1)
+		expect(1).not.toBe(1)
+	})
+	test('some test 3', () => {
+		expect('feature-1').toBe('feature-1')
+	})
+	test('some test 4', () => {
+		expect(3).not.toBe('feature-1')
 	})
 })
